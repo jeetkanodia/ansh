478c61e40d178b8713a9045496d4db2a52253ee2
jeet kanodia <jeetkanodia@gmail.com>
2024-05-16 16:23:12 +0530

    slider made responsive

--- src/components/Bottom.jsx
+++ src/components/Bottom.jsx
@@ -3,6 +3,12 @@ import Sliderr from "./Sliderr";
 const Bottom = () => {
   return (
     <div>
+      <h1>Executive Team</h1>
+      <h1>
+        A seasoned Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Aliquam, quasi!
+      </h1>
+
       <Sliderr />
     </div>
   );
--- src/components/Sliderr.jsx
+++ src/components/Sliderr.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
@@ -10,60 +10,106 @@ const data = [
     img: slider1,
     title: "Hasaan AlQurashi",
     sub: "Chief Executive Officer",
-    desc: "Hasaan AlQurashi is the Chief Executive Officer of AlQurashi Group. He is responsible for the overall management of the Group and its strategic direction. Hasaan has been with AlQurashi Group since 2018 and has held various leadership roles within the Group. Prior to joining AlQurashi Group, Hasaan was a Partner at McKinsey & Company, where he advised clients on strategy, operations, and organization. Hasaan holds a Bachelor’s degree in Economics from the University of Chicago and an MBA from Harvard Business School.",
+    desc: "Hassan has been the Executive Vice President of Operations at RHC since 2014. He has 20+ years of experience; he joined the company in 2002 as Director of Marketing and Public Relations. His previous experience was in the field of Marketing and Public Relations at Al-Hayat Newspaper and Wirily Wireless Company.",
   },
   {
     id: 2,
     img: slider1,
     title: "Hasaan AlQurashi",
     sub: "Chief Executive Officer",
-    desc: "Hasaan AlQurashi is the Chief Executive Officer of AlQurashi Group. He is responsible for the overall management of the Group and its strategic direction. Hasaan has been with AlQurashi Group since 2018 and has held various leadership roles within the Group. Prior to joining AlQurashi Group, Hasaan was a Partner at McKinsey & Company, where he advised clients on strategy, operations, and organization. Hasaan holds a Bachelor’s degree in Economics from the University of Chicago and an MBA from Harvard Business School.",
+    desc: "Hassan has been the Executive Vice President of Operations at RHC since 2014. He has 20+ years of experience; he joined the company in 2002 as Director of Marketing and Public Relations. His previous experience was in the field of Marketing and Public Relations at Al-Hayat Newspaper and Wirily Wireless Company.",
   },
   {
     id: 3,
     img: slider1,
     title: "Hasaan AlQurashi",
     sub: "Chief Executive Officer",
-    desc: "Hasaan AlQurashi is the Chief Executive Officer of AlQurashi Group. He is responsible for the overall management of the Group and its strategic direction. Hasaan has been with AlQurashi Group since 2018 and has held various leadership roles within the Group. Prior to joining AlQurashi Group, Hasaan was a Partner at McKinsey & Company, where he advised clients on strategy, operations, and organization. Hasaan holds a Bachelor’s degree in Economics from the University of Chicago and an MBA from Harvard Business School.",
+    desc: "Hassan has been the Executive Vice President of Operations at RHC since 2014. He has 20+ years of experience; he joined the company in 2002 as Director of Marketing and Public Relations. His previous experience was in the field of Marketing and Public Relations at Al-Hayat Newspaper and Wirily Wireless Company.",
   },
   {
     id: 4,
     img: slider1,
     title: "Hasaan AlQurashi",
     sub: "Chief Executive Officer",
-    desc: "Hasaan AlQurashi is the Chief Executive Officer of AlQurashi Group. He is responsible for the overall management of the Group and its strategic direction. Hasaan has been with AlQurashi Group since 2018 and has held various leadership roles within the Group. Prior to joining AlQurashi Group, Hasaan was a Partner at McKinsey & Company, where he advised clients on strategy, operations, and organization. Hasaan holds a Bachelor’s degree in Economics from the University of Chicago and an MBA from Harvard Business School.",
+    desc: "Hassan has been the Executive Vice President of Operations at RHC since 2014. He has 20+ years of experience; he joined the company in 2002 as Director of Marketing and Public Relations. His previous experience was in the field of Marketing and Public Relations at Al-Hayat Newspaper and Wirily Wireless Company.",
   },
 ];
 
 const Sliderr = () => {
+  const [imageIndex, setImageIndex] = useState(1);
   var settings = {
-    dots: true,
+    dots: false,
+    speed: 200,
+    initialSlide: 0,
+    className: "center",
+    // nextArrow: <NextArrow />,
+    // prevArrow: <PrevArrow />,
+    centerMode: true,
+
+    centerPadding: "60px",
+    focusOnSelect: true,
     infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
+    slidesToShow: 3,
+    beforeChange: (current, next) => {
+      console.log(current);
+      setImageIndex(next);
+    },
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
   return (
-    <Slider {...settings}>
-      <div>
-        <h3>1</h3>
-      </div>
-      <div>
-        <h3>2</h3>
-      </div>
-      <div>
-        <h3>3</h3>
-      </div>
-      <div>
-        <h3>4</h3>
-      </div>
-      <div>
-        <h3>5</h3>
-      </div>
-      <div>
-        <h3>6</h3>
-      </div>
-    </Slider>
+    <div className="slider-container">
+      <Slider {...settings}>
+        {data.map((item) => (
+          <SliderCard
+            active={imageIndex + 1 === item.id}
+            key={item.id}
+            img={item.img}
+            title={item.title}
+            sub={item.sub}
+            desc={item.desc}
+          />
+        ))}
+      </Slider>
+    </div>
+  );
+};
+
+const SliderCard = ({ img, title, sub, desc, active }) => {
+  return (
+    <div className={`m-5 ml-10 max-w-sm ${active ? "" : "opacity-40"}`}>
+      <img
+        src={img}
+        alt="slider img"
+        className={`${active ? "w-[105%]" : " "}`}
+      />
+      <h1 className="text-[#154466] SomarSemiBold text-xl mt-4">{title}</h1>
+      <h3 className="text-[#154466] mt-2">{sub}</h3>
+      <p className={`text-[#3C729C] mt-8 ${active ? "show" : "hidden"}`}>
+        {desc}
+      </p>
+    </div>
   );
 };
 
